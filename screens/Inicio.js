30cb2f33f5c303b7a0d3e41dc19b6a6563d94f5e
uricherni <102042188+uricherni@users.noreply.github.com>
2022-08-19 10:01:17 -0300

    Update Inicio.js

--- screens/Inicio.js
+++ screens/Inicio.js
@@ -12,7 +12,7 @@ const Inicio = ({ navigation }) => {
         <Text style={styles.Texto}>Bienvenido a Petfy</Text>
             <Image style={styles.img} source={require('../assets/Inicio.png')}></Image>
             <TouchableOpacity style={styles.Enviar} onPress={login}>
-                    <Text>Continuar</Text>
+                    <Text>Continuar    </Text>
             </TouchableOpacity>
         </View>
         
